7a9a4db44a4f10f689ca284b6510d1249ab25597
akbanana7 <116113592+akbanana7@users.noreply.github.com>
2023-11-03 14:14:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// variable
+
+var calc1 = calculate(3, 5)
+
+// calculate
+function calculate(number1, number2) {
+    document.getElementById("test1").innerHTML = calc1;
+    return number1 + number2;
+}
+
+if (document.getElementById("test1") = "Not Working") {
+    window.alert("Not Working")
+} else {
+    window.alert("Kinda Works")
+}
+
+function test() {
+    window.open("https://www.youtube.com")
+    while(true){
+        window.alert("Alert/Warning Javascript Works")
+    }
+}
